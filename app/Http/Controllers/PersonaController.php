a8bcd20448c8489058dc65da287c372a85c2594c
proasis <38759923+proasis@users.noreply.github.com>
2018-06-28 16:06:01 -0600

    BD01.00 - SF01.00 BETA WebService: ...versión inicial...

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -4,53 +4,53 @@ use App\Persona;
 
 class PersonaController extends Controller {
 
-    public function index()
-    {
-    	return response()->json(['datos'=>Persona::all()],200);
+  public function index()
+  {
+  	return response()->json(['datos'=>Persona::all()],200);
+  }
+
+  public function update(Request $request, $id)
+  {
+    $metodo=$request->method();
+  	$persona=Persona::find($id);
+  	if($metodo==="PATCH"){
+  		$nombre=$request->get('nombre');
+  		if($nombre!=null && $nombre!=' '){
+  			$persona->nombre=$nombre;
+  			$persona->apellido=$apellido;
+  			$persona->fechaNacimiento=$fechaNacimiento;
+  		}
+  		$persona->save();
+  		return response()->json(['mensaje'=>'Registro actualizado','codigo'=>'204'],204);
     }
+  	$nombre=$request->get('nombre');
+  	$apellido=$request->get('apellido');
+  	if (!$nombre || !$apellido)
+  	{
+  		return response()->json(['mensaje'=>'El nombre o apellido no pueden ser nulos','codigo'=>'400'],400);
+  	}
+  	$persona->nombre=$nombre;
+  	$persona->apellido=$apellido;
+  	$persona->save();
+  	return response()->json(['mensaje'=>'Persona actualizada','codigo'=>'200'],200);
+  }
 
-    public function update(Request $request, $id)
-    	{
-    		$metodo=$request->method();
-    		$persona=Persona::find($id);
-    		if($metodo==="PATCH"){
-    			$nombre=$request->get('nombre');
-    			if($nombre!=null && $nombre!=' '){
-    				$persona->nombre=$nombre;
-    				$persona->apellido=$apellido;
-    				$persona->fechaNacimiento=$fechaNacimiento;
-    			}
-    			$persona->save();
-    			return response()->json(['mensaje'=>'Registro actualizado','codigo'=>'204'],204);
-    		}
-    		$nombre=$request->get('nombre');
-    		$apellido=$request->get('apellido');
-    		if (!$nombre || !$apellido)
-    		{
-    			return response()->json(['mensaje'=>'El nombre o apellido no pueden ser nulos','codigo'=>'400'],400);
-    		}
-    		$persona->nombre=$nombre;
-    		$persona->apellido=$apellido;
-    		$persona->save();
-    		return response()->json(['mensaje'=>'Persona actualizada','codigo'=>'200'],200);
-    	}
+  public function store(Request $request)
+  {
+  	if(!$request->get('dui') || !$request->get('nombre') || !$request->get('apellido') || !$request->get('fechaNacimiento')){
+  		return response()->json(['mensaje'=>'Datos incompletos'],400);
+  	}
+  	Persona::create($request->all());
+  	return response()->json(['mensaje'=>'Persona creada'],201);
+  }
 
-      public function store(Request $request)
-    	{
-    		if(!$request->get('dui') || !$request->get('nombre') || !$request->get('apellido') || !$request->get('fechaNacimiento')){
-    			return response()->json(['mensaje'=>'Datos incompletos'],400);
-    		}
-    		Persona::create($request->all());
-    		return response()->json(['mensaje'=>'Persona creada'],201);
-    	}
-
-      public function destroy($id)
-    	{
-    		$persona=Persona::find($id);
-    		if(!$persona){
-    			return response()->json(['mensaje'=>'Persona no existe','codigo'=>'400'],400);
-    		}
-    		$persona->delete();
-    		return response()->json(['mensaje'=>'Persona eliminada','codigo'=>'204'],204);
-    	}
+  public function destroy($id)
+  {
+  	$persona=Persona::find($id);
+  	if(!$persona){
+  		return response()->json(['mensaje'=>'Persona no existe','codigo'=>'400'],400);
+  	}
+  	$persona->delete();
+  	return response()->json(['mensaje'=>'Persona eliminada','codigo'=>'204'],204);
+  }
 }
